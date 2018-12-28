7f91a2f430e11f708497c73084383e411a8538a4
Jonathan McChesney <jmcchesney01@qub.ac.uk>
2018-12-28 18:02:44 +0000

    update scripts

--- Experiments/PocketSphinx/docker/execute.sh
+++ Experiments/PocketSphinx/docker/execute.sh
@@ -6,7 +6,7 @@ cd pocketsphinx/
 pocketsphinx_continuous -infile b0427.wav -logfn /dev/null
 echo "Starting Upload to S3 Bucket..."
 chmod 777 s3Upload.py
-/usr/bin/time python s3Upload.py results.txt speech.txt
+/usr/bin/time python s3Upload.py result.txt psphinx-result.txt
 echo "Done"
 exit
 
