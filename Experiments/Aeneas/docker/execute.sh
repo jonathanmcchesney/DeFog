130ddb12e1d281e73f4a388cd0087c233cfed04e
Jonathan McChesney <jmcchesney01@qub.ac.uk>
2019-01-01 16:36:02 +0000

    update dockerfile and execute scripts for aeneas

--- Experiments/Aeneas/docker/execute.sh
+++ Experiments/Aeneas/docker/execute.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cd ~/Experiments/Aeneas/aeneas
-chmod 777 darknet
 python -m aeneas.tools.execute_task     /mnt/assets/aeneasaudio.mp3     /mnt/assets/aeneastext.xhtml     "task_language=eng|os_task_file_format=smil|os_task_file_smil_audio_ref=audio.mp3|os_task_file_smil_page_ref=page.xhtml|is_text_type=unparsed|is_text_unparsed_id_regex=f[0-9]+|is_text_unparsed_id_sort=numeric"     map.smil
 echo "Starting Upload to S3 Bucket..."
 chmod 777 s3Upload.py
