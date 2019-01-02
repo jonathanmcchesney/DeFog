bdb5b165b848d68b710e61ff8ebe3aa533f411b5
Jonathan McChesney <jmcchesney01@qub.ac.uk>
2019-01-02 16:09:30 +0000

    update YOLO results

--- Experiments/YOLO/docker/execute.sh
+++ Experiments/YOLO/docker/execute.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+rm /mnt/results/results.txt
+
 cd ~/Experiments/YOLO/yolo
 chmod 777 darknet
-
 
 echo "Starting YOLO detection..." | tee results.txt
 start=$(date +%s.%N)
 ./darknet detect cfg/yolov3-tiny.cfg yolov3-tiny.weights /mnt/assets/yoloimage.jpg
 end=$(date +%s.%N)
 runtime=$( echo "$end - $start" | bc -l )
-echo "Computation: completed in $runtime secs" | tee results.txt
+echo "Computation: completed in $runtime secs" | tee -a results.txt
 
 
 echo "Starting Upload to S3 Bucket..."
@@ -19,7 +20,7 @@ start=$(date +%s.%N)
 python s3Upload.py predictions.png predict.png
 end=$(date +%s.%N)
 runtime=$( echo "$end - $start" | bc -l )
-echo "Upload to S3 bucket: completed in $runtime seconds" | tee -a results.txt
+echo "Upload to S3 bucket: completed in $runtime secs" | tee -a results.txt
 cat results.txt >> /mnt/results/results.txt
 
 exit
