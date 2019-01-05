044a5a689ec7eae266e2313a7374f68b55dec5b9
Jonathan McChesney <jmcchesney01@qub.ac.uk>
2019-01-05 13:14:54 +0000

    update metrics array

--- Experiments/YOLO/docker/execute.sh
+++ Experiments/YOLO/docker/execute.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 rm /mnt/results/results.txt
+rm /mnt/results/newResults.txt
 
 cd ~/Experiments/YOLO/yolo
 chmod 777 darknet
+
+declare -g metricsValues=('NA' 'NA' 'NA' 'NA' 'NA' 'NA' 'NA' 'NA' 'NA')
 
 start=$(date +%s.%N)
 ./darknet detect cfg/yolov3-tiny.cfg yolov3-tiny.weights /mnt/assets/yoloimage.jpg
@@ -11,6 +14,7 @@ end=$(date +%s.%N)
 runtime=$( echo "$end - $start" | bc -l )
 echo "Computation: completed in $runtime secs" | tee -a results.txt
 
+metricsValues[1]=$runtime
 
 echo "Starting Upload to S3 Bucket..."
 chmod 777 s3Upload.py
@@ -20,6 +24,11 @@ python s3Upload.py predictions.png predict.png
 end=$(date +%s.%N)
 runtime=$( echo "$end - $start" | bc -l )
 echo "Upload to S3 bucket: completed in $runtime secs" | tee -a results.txt
+
+metricsValues[2]=$runtime
+
+echo ${metricsValues[@]} >> /mnt/results/newResults.txt
+
 cat results.txt >> /mnt/results/results.txt
 
 exit
