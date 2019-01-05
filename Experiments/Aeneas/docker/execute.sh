f2b1b0aec293a0fb58c35a95c2dfb83dc16ef679
Jonathan McChesney <jmcchesney01@qub.ac.uk>
2019-01-05 18:30:30 +0000

    update program scripts

--- Experiments/Aeneas/docker/execute.sh
+++ Experiments/Aeneas/docker/execute.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 rm /mnt/results/results.txt
+rm /mnt/results/arrresult.txt
 
 cd ~/Experiments/Aeneas/aeneas
 
 export PYTHONIOENCODING=UTF-8
+
+metricsValues=("NA" "NA" "NA" "NA" "NA" "NA" "NA" "NA" "NA")
 
 start=$(date +%s.%N)
 python -m aeneas.tools.execute_task     /mnt/assets/aeneasaudio.mp3     /mnt/assets/aeneastext.xhtml     "task_language=eng|os_task_file_format=smil|os_task_file_smil_audio_ref=audio.mp3|os_task_file_smil_page_ref=page.xhtml|is_text_type=unparsed|is_text_unparsed_id_regex=f[0-9]+|is_text_unparsed_id_sort=numeric"     map.smil
 end=$(date +%s.%N)
 runtime=$( echo "$end - $start" | bc -l )
 echo "Computation: completed in $runtime secs" | tee -a results.txt
+
+metricsValues[1]=$runtime
 
 echo "Starting Upload to S3 Bucket..."
 chmod 777 s3Upload.py
@@ -20,7 +25,11 @@ python s3Upload.py map.smil aeneeas-map.smil
 end=$(date +%s.%N)
 runtime=$( echo "$end - $start" | bc -l )
 echo "Upload to S3 bucket: completed in $runtime secs" | tee -a results.txt
+
+metricsValues[2]=$runtime
+
 cat results.txt >> /mnt/results/results.txt
+echo ${metricsValues[@]} >> /mnt/results/arrresult.txt
 
 exit
 
--- Experiments/PocketSphinx/docker/execute.sh
+++ Experiments/PocketSphinx/docker/execute.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
 rm /mnt/results/results.txt
+rm /mnt/results/arrresult.txt
 
 cd ~/Experiments/PocketSphinx/sphinxbase/ 
 export LD_LIBRARY_PATH=/usr/local/lib
 cd pocketsphinx/
+
+metricsValues=("NA" "NA" "NA" "NA" "NA" "NA" "NA" "NA" "NA")
 
 start=$(date +%s.%N)
 pocketsphinx_continuous -infile /mnt/assets/psphinx.wav -logfn /dev/null
 end=$(date +%s.%N)
 runtime=$( echo "$end - $start" | bc -l )
 echo "Computation: completed in $runtime secs" | tee -a results.txt
+
+metricsValues[1]=$runtime
 
 echo "Starting Upload to S3 Bucket..."
 chmod 777 s3Upload.py
@@ -20,7 +25,11 @@ python s3Upload.py result.txt psphinx-result.txt
 end=$(date +%s.%N)
 runtime=$( echo "$end - $start" | bc -l )
 echo "Upload to S3 bucket: completed in $runtime secs" | tee -a results.txt
+
+metricsValues[2]=$runtime
+
 cat results.txt >> /mnt/results/results.txt
+echo ${metricsValues[@]} >> /mnt/results/arrresult.txt
 
 exit
 
